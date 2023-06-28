d90cb923d518c69c0822316d86e1af8fdafc8551
andreacassini <andrea1316@hotmail.it>
2023-06-28 10:59:14 +0200

    Header done fix installing boostrap and axios

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,8 +10,47 @@ export default {
 }
 </script>
 <template>
-
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-6 d-flex">
+                <div class="big-circle"></div>
+                <div class="small-circle red">
+                </div>
+                <div class="small-circle yellow"></div>
+                <div class="small-circle green"></div>
+            </div>
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
 
+.big-circle{
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+    background: rgb(0,45,240);
+    border: 4px solid white;
+    background: linear-gradient(297deg, rgba(0,45,240,1) 28%, rgba(255,255,255,1) 100%);
+}
+
+.small-circle{
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    border: 4px solid white;
+    margin-left: 1em;
+}
+.red{
+    background: rgb(240,0,0);
+    background: linear-gradient(297deg, rgba(240,0,0,1) 10%, rgba(255,255,255,1) 95%);
+}
+
+.yellow{
+    background: rgb(226,240,0);
+    background: linear-gradient(297deg, rgba(226,240,0,1) 28%, rgba(255,255,255,1) 90%);
+}
+.green{
+    background: rgb(12,175,11);
+    background: linear-gradient(297deg, rgba(12,175,11,1) 28%, rgba(255,255,255,1) 90%);
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -3,5 +3,4 @@ import { reactive } from "vue";
 export const store = reactive({
     apiUrl: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=12',
     listOfPokemon: [],
-    title: 'Pokédex BOOLEAN'
 })
