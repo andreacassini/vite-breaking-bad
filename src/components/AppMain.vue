fb82530861d15890c563fd62f7b39fed596174fd
andreacassini <andrea1316@hotmail.it>
2023-06-28 11:11:51 +0200

    style AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,8 +15,28 @@ export default {
 }
 </script>
 <template>
+ <div class="container my-4">
+        <div class="row row-content">
 
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
-
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2.7em 1.5em;
+    background-color: white;
+    border-radius: 18px;
+    .row-content{
+        background-color: #4C5157;
+        justify-content: center;
+        padding: 2em 1em;
+        height: 500px;
+        overflow: auto;
+        width: 100%;
+        
+    }
+}
 </style>
