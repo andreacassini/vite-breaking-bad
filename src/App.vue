b6d70e4a4ac2e5f1e1cd36207181821a7aa23b6d
andreacassini <andrea1316@hotmail.it>
2023-06-28 10:38:47 +0200

    macrostructure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <script>
+import axios from 'axios';
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+
+import { store } from './store.js';
+
 export default {
-  
+  components: {
+    AppHeader,
+    AppMain
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+  mounted() {
+    axios.get(store.apiUrl).then((response) => {
+      store.listOfPokemon = response.data.docs;
+    })
+  },
 }
 </script>
-
 <template>
   <div>
-    
+    <AppHeader />
+    <AppMain />
   </div>
 </template>
-
 <style lang="scss">
-  
+@use './styles/generals.scss' as *;
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+import AppCards from './AppCards.vue'
+
+import { store } from '../store.js';
+
+export default {
+    components:{
+        AppCards
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+<template>
+
+</template>
+<style lang="scss" scoped>
+
+</style>
