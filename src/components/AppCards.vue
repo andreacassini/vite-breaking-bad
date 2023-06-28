e685cb4c73ff70203c8fad5ec7c3206a59458a1c
andreacassini <andrea1316@hotmail.it>
2023-06-28 11:51:38 +0200

    first es done

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,10 +1,41 @@
 <script>
 export default {
+    props:{
+        myPokemon: Object
+    }
 }
 </script>
 <template>
-    
+    <div class="card" :class="myPokemon.type1 === 'Grass'? 'grass' : myPokemon.type1 === 'Water' ? 'water' : myPokemon.type1 === 'Fire'? 'fire': ''">
+        <img :src="myPokemon.imageUrl" :alt="myPokemon.name">
+        <div>{{ myPokemon.number }}</div>
+        <p><strong>{{ myPokemon.name }}</strong></p>
+        <div>{{ myPokemon.type1 }}</div>
+    </div>
 </template>
 <style lang="scss">
+img{
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+}
 
+.card{
+    display: flex;
+    align-items: center;
+    margin: 1em 0em;
+    padding: 25px 20px;
+}
+
+.grass{
+    background-color: #DEFDE0 !important
+}
+
+.fire{
+    background-color: #FDDFDF !important
+}
+
+.water{
+    background-color: #DEF3FD !important
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,7 +17,9 @@ export default {
 <template>
  <div class="container my-4">
         <div class="row row-content">
-
+            <div v-for="(pokemon, index) in store.listOfPokemon" :key="index" class="col-6 col-md-4 col-lg-3">
+                <AppCards :myPokemon="pokemon" />
+            </div>
         </div>
     </div>
 </template>
@@ -32,11 +34,11 @@ export default {
     .row-content{
         background-color: #4C5157;
         justify-content: center;
-        padding: 2em 1em;
+        padding: 0.8em 0em;
         height: 500px;
         overflow: auto;
         width: 100%;
-        
     }
 }
+
 </style>
